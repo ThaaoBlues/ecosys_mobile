f7250a194b46f172b7693d10e571ed523670b955
thaaoblues <thaaoblues81@gmail.com>
2024-06-27 22:49:15 +0200

    magasin is working :DDD

--- app/src/main/java/com/qsync/qsync/MagasinFragment.java
+++ app/src/main/java/com/qsync/qsync/MagasinFragment.java
@@ -8,23 +8,40 @@
 
 package com.qsync.qsync;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.bumptech.glide.Glide;
 import com.qsync.qsync.databinding.FragmentMagasinBinding;
 import com.qsync.qsync.databinding.FragmentSynchronisationsBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MagasinFragment extends Fragment {
 
-
+    private static String TAG = "Qsycn Server : MagasinFragment";
     private FragmentMagasinBinding binding;
     @Nullable
     @Override
@@ -45,7 +62,72 @@ public class MagasinFragment extends Fragment {
                         .navigate(R.id.action_fragment_magasin_to_LargageAerienFragment)
         );
 
+
+        fetchConfig(binding.fragmentMagasinConstraintLayout);
+
+
+
     }
+
+    private void fetchConfig(View view) {
+        new Thread(() -> {
+            try {
+                URL url = new URL("https://raw.githubusercontent.com/ThaaoBlues/qsync/master/magasin_database.json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                StringBuilder json = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    json.append(line);
+                }
+                reader.close();
+
+                JSONObject data = new JSONObject(json.toString());
+                //Log.d(TAG,data.get("tout_en_un_configs").toString());
+                JSONArray arr = data.getJSONArray("tout_en_un_configs");
+                getActivity().runOnUiThread(() -> fillMagasin(view, getContext(),arr));
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error fetching config file", e);
+            }
+        }).start();
+    }
+
+    private void fillMagasin(View view, Context context, JSONArray data) {
+        try {
+            LinearLayout container = view.findViewById(R.id.container);
+
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject config = data.getJSONObject(i);
+                Log.d(TAG,config.getJSONArray("SupportedPlatforms").toString());
+                if (config.getJSONArray("SupportedPlatforms").toString().contains("Android")) {
+                    View card = LayoutInflater.from(context).inflate(R.layout.card_layout, container, false);
+
+                    ImageView image = card.findViewById(R.id.app_image);
+                    TextView title = card.findViewById(R.id.card_title);
+                    TextView description = card.findViewById(R.id.card_description);
+                    Button installButton = card.findViewById(R.id.install_button);
+
+                    Glide.with(this).load(config.getString("AppIconURL")).into(image);
+                    title.setText(config.getString("AppName"));
+                    description.setText(config.getString("AppDescription"));
+
+                    String appDownloadUrl = config.getString("AppDownloadUrl");
+                    installButton.setOnClickListener(v -> {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(appDownloadUrl));
+                        startActivity(intent);
+                    });
+
+                    container.addView(card);
+                }
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Error parsing JSON", e);
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
