95dbdfef4fc0d66db650aed214df688fd06bfdb7
thaaoblues <thaaoblues81@gmail.com>
2024-09-17 21:31:28 +0200

    version change

--- app/src/main/java/com/ecosys/ecosys/Globals.java
+++ app/src/main/java/com/ecosys/ecosys/Globals.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class Globals {
 
 
-    public static final String VERSION = "0.0.4-Beta";
+    public static final String VERSION = "0.0.5-Beta";
 
     public static class QEvent {
         public String Flag;
